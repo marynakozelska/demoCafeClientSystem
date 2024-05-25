31da2c2367b22131422ddd24bac0c9cd3133c63b
Maryna Kozelska <marynakozelska@gmail.com>
2024-05-25 13:49:08 +0300

    Add docker support.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:17-jdk-alpine
 
 WORKDIR /opt/cafe-client-system
 
-ADD target/demoCafeClientSystem-0.0.1-SNAPSHOT.jar cafe-client-system.jar
+ADD target/demoCafeClientSystem-0.0.1-SNAPSHOT.jar root.jar
 EXPOSE 8080
 
-CMD java -jar cafe-client-system.jar
+CMD java -jar root.jar
--- src/main/java/com/example/democafeclientsystem/config/SecurityConfig.java
+++ src/main/java/com/example/democafeclientsystem/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost:30042"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "isRefreshToken"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
